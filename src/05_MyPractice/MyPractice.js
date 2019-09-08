1f11b3fdda556e9a13cdde593674d16d9f9a3dde
kirbycampbell <jkirbycampbell@gmail.com>
2019-09-08 16:09:50 -0700

    Updates

--- src/05_MyPractice/MyPractice.js
+++ src/05_MyPractice/MyPractice.js
@@ -72,12 +72,33 @@ const MyPractice = props => {
         </div>
         <div className="Right-Maj-Col">
           <div className="Button-Col">
-            <div className="btn-col-right">Button1</div>
-            <div className="btn-col-right">Button1</div>
+            <div className="btn-col-right">Cartilage Restoration</div>
+            <div className="btn-col-right">Hip Arthroscopy</div>
 
-            <div className="btn-col-right">Button1</div>
+            <div className="btn-col-right">Shoulder Arthroscopy</div>
 
-            <div className="btn-col-right">Button1</div>
+            <div className="btn-col-right">
+              Knee Arthroscopy/ Ligament Reconstruction
+            </div>
+          </div>
+          <div className="Button-Col-2">
+            <div className="btn-col-right-2">Patient Forms</div>
+            <div className="btn-col-right-2">News Publications</div>
+            <div className="btn-col-right-2">Patient Story</div>
+          </div>
+          <div className="Content-Selection">
+            <div className="quote">"</div>
+            He went over my hip problem in detail and explained all of my
+            options. I decided on surgery and everything has been better than I
+            hoped. It was great to have a doctor that listens to me and I'm
+            impressed with how good my hip feels! <br /> - Patient Smith
+          </div>
+          <div className="Content-Selection">
+            <div className="quote">"</div>
+            Excellent service and very friendly attitude. We were very pleased
+            with how well he explained the plan of care and surgery went very
+            well.
+            <br /> - Patient Jones
           </div>
         </div>
       </div>
